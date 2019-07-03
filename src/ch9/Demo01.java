b0e2432acf71aa806a9c688a4359e2de308aeda8
wangyuliang@abupdate.com <wangyuliang@abupdate.com>
2019-07-03 20:08:16 +0800

    feat: 新增线程

---
+++ src/ch9/Demo01.java
@@ -0,0 +1,37 @@
+package ch9;
+
+import java.util.Date;
+
+/**
+ * @author: yuliangw
+ * @version: v2.0
+ * @description: ch9
+ * @date:2019/7/3
+ */
+public class Demo01 {
+        public static void main(String[] args) {
+            ThreadStop ts = new ThreadStop();
+            ts.start();
+            try {
+                Thread.sleep(3000);
+                // ts.stop();
+                ts.interrupt();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+}
+
+class ThreadStop extends Thread {
+    @Override
+    public void run() {
+        System.out.println("开始执行：" + new Date());
+            System.out.println(1111);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("结束执行：" + new Date());
+    }
+}
---
+++ src/ch9/Demo02.java
@@ -0,0 +1,46 @@
+package ch9;
+
+import java.util.Date;
+
+/**
+ * @author: yuliangw
+ * @version: v2.0
+ * @description: 应该重置一下中断标示，因为抛出异常后，中断标示会自动清除掉！
+ * @date:2019/7/3
+ */
+public class Demo02 {
+
+    public static void main(String[] args) {
+        ThreadStop1 ts = new ThreadStop1();
+        ts.start();
+        // 你超过三秒不醒过来，我就干死你
+        try {
+             Thread.sleep(3000);
+             // ts.
+            // ts.stop();
+             ts.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+class ThreadStop1 extends Thread {
+    @Override
+    public void run() {
+
+        Thread th = Thread.currentThread();
+        while(true) {
+            if(th.isInterrupted()) {
+                break;
+            }
+            // 省略业务代码无数
+            try {
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
